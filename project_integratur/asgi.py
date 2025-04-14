0f1071446e961a83f201298539ef6ca9297e6cb2
unknown <mdudarodrigues.contato@gmail.com>
2025-04-13 22:03:16 -0300

    att project

--- project_integratur/asgi.py
+++ project_integratur/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bancosdedado project.
+ASGI config for project_integratur project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bancosdedado.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_integratur.settings')
 
 application = get_asgi_application()
--- project_integratur/urls.py
+++ project_integratur/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bancosdedado project.
+URL configuration for project_integratur project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
